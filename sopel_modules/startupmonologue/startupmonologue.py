9a50be0b2dfaf6257e2c57f3a97a4647988c7cde
deathbybandaid <sam@deathbybandaid.net>
2019-04-12 16:21:39 -0400

    Monologue should work

--- sopel_modules/startupmonologue/startupmonologue.py
+++ sopel_modules/startupmonologue/startupmonologue.py
@@ -4,6 +4,22 @@ from __future__ import unicode_literals, absolute_import, division, print_functi
 
 from sopel import module
 
+import sopel_modules.osd
+
+import time
+
+try:
+    from sopel_modules.botevents.botevents import *
+    botevents_installed = True
+except ImportError:
+    botevents_installed = False
+
+try:
+    from sopel_modules.commandsquery.commandsquery import *
+    commandsquery_installed = True
+except ImportError:
+    commandsquery_installed = False
+
 
 def configure(config):
     pass
@@ -13,6 +29,39 @@ def setup(bot):
     pass
 
 
-@module.commands('helloworld')
-def hello_world(bot, trigger):
-    bot.say('Hello, world!')
+@module.event('001')
+@module.rule('.*')
+@module.thread(True)
+def bot_startup_monologue(bot, trigger):
+
+    if botevents_installed:
+        while not check_bot_events(bot, ["connected"]):
+            pass
+    else:
+        while not len(bot.channels.keys()) > 0:
+            pass
+        time.sleep(1)
+
+    # Startup
+    bot.osd(" is now starting. Please wait while I load my configuration.", bot.channels.keys(), 'ACTION')
+
+    startupcomplete = [bot.nick + " startup complete"]
+    if commandsquery_installed:
+
+        availablecomsnum, availablecomsfiles = 0, 0
+
+        for commandstype in bot.memory['Sopel-CommandsQuery'].keys():
+            availablecomsnum += len(bot.memory['Sopel-CommandsQuery'][commandstype].keys())
+            availablecomsfiles += bot.memory['Sopel-CommandsQuery'][commandstype + "_count"]
+
+        startupcomplete.append("There are " + str(availablecomsnum) + " commands available in " + str(availablecomsfiles) + " files.")
+
+    if botevents_installed:
+        while not check_bot_events(bot, ["startup_complete"]):
+            pass
+
+    # Announce to chan, then handle some closing stuff
+    bot.osd(startupcomplete, bot.channels.keys())
+
+    if botevents_installed:
+        startup_bot_event(bot, "Sopel-StartupMonologue")
